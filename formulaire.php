5c0e78fa448297bab7d764728adbea366b0c0b3e
kevin-JOAQUIM <kevin.marques.joaquim@mail.com>
2017-09-14 22:22:00 +0200

    site dj

---
+++ formulaire.php
@@ -0,0 +1,26 @@
+<form action="modelformulaire.php" method="post">
+
+  <div class="input-group">
+    <span class="input-group-addon col-lg-2">Nom</span>
+    <input id="nom" type="text" class="form-control" name="nom" placeholder="nom"> 
+
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon col-lg-2">Prénom</span>
+    <input id="prenom" type="text" class="form-control" name="prenom" placeholder="Prénom">
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon col-lg-2">mail</span>
+    <input id="mail" type="mail" class="form-control" name="mail" placeholder="mail@mail.com">
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon col-lg-2">Téléphone</span>
+    <input id="tel" type="tel" class="form-control" name="tel" placeholder="Téléphone">
+  </div>
+  <div class="input-group">
+    <span class="input-group-addon col-lg-2">Descriptif</span>
+    <input id="description" type="text" class="form-control" name="msg" placeholder="Additional Info">
+  </div>
+    <button type="submit" class="btn btn-primary pull-right">Envoyer</button>
+</form>
+
---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>DJ Kevin Joaquim</title>
+
+    
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Kevin JOAQUIM</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#soundcloud">soundcloud</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Portofolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+    </nav>
+
+    <header class="masthead">
+      <div class="header-content">
+        <div class="header-content-inner">
+            <a class="btn btn-primary " href="#page-top"><h1> DJ Kevin Joaquim</h1></a>
+          <hr>
+          <p>House / Electro / AfroBeats / Latina / Dancehall</p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#contact"> Contactez nous</a>
+        </div>
+      </div>
+    </header>
+
+    <section id="soundcloud"  >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-black">Mix Live</h2>
+            <hr class="light">
+            <p class="text-black"> DJ depuis 10 ans
+            <br>
+                Retrouvez Kevin JOAQUIM sur son soundcloud pour suivre ses mixtapes et production
+            <br>
+            <br>
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/250191746&amp;color=0066cc&amp;auto_play=true&amp;hide_related=false&amp;show_cMix liveomments=true&amp;show_user=true&amp;show_reposts=false"></iframe></p>
+            
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="https://soundcloud.com/djkevinjoaquim/tracks" target="_blank"> Soundcloud<a>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services" style="background-color:navy;" >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-white">Service</h2>
+            <hr class="primary">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
+            <h3 class="section-heading text-white">Mariage</h2>
+         <br>
+              <p class="text-white">Our templates are updated regularly so they don't break.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+            <h3 class="section-heading text-white">Mariage</h2>
+         <br>
+              <p class="text-white">Our templates are updated regularly so they don't break.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+            <h3 class="section-heading text-white">Mariage</h2>
+         <br>
+              <p class="text-white">Our templates are updated regularly so they don't break.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+            <h3 class="section-heading text-white">Mariage</h2>
+         <br>
+              <p class="text-white">Our templates are updated regularly so they don't break.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid">
+        <div class="row no-gutter popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/festival.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/festival.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Festival
+                  </div>
+                  <div class="project-name">
+                    House Deep Electro
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/assomption.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/assomption.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Soirée étudiante
+                  </div>
+                  <div class="project-name">
+                    Complet
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/platine.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/platine.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Facebook
+                  </div>
+                  <div class="project-name">
+                    #DJKEVINJOAQUIM
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/soiree.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/soiree.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Soundcloud
+                  </div>
+                  <div class="project-name">
+                    #Soundcloud
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/domicile.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/domicile.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Webradio
+                  </div>
+                  <div class="project-name">
+                    live mixtape
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/light.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/light.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Live
+                  </div>
+                  <div class="project-name">
+                    
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+     <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Contact</h2>
+            <hr class="primary">
+            <p> Interessé par une prestation? Vous organisez un évènement? Vous êtes à la recherche d'un DJ?</p>
+            <p>N'hésitez pas à me contacter en remplissant le formulatire ci-dessous :</p>
+            
+            <?php include('formulaire.php'); ?>
+
+          </div>
+        </div>
+          <div class="col-lg-8 mx-auto text-center">
+              <p> Copyright Kevin JOAQUIM by web-Easy</p>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
